57abde0294c09a55015f415f0a2d07b38da58f12
hshristov18 <56866312+hshristov18@users.noreply.github.com>
2022-06-17 19:03:20 +0300

    Adding new project database

---
+++ SACPdatabase.sql
@@ -0,0 +1,8 @@
+create database youtube;
+use youtube;
+create table users(id int primary key auto_increment, uname varchar(50), upwd varchar(50), uemail varchar(50), umobile varchar(20));
+desc users;
+
+select * from users
+
+

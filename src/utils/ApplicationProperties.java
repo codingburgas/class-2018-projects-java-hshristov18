1e63e32f712773711e1bd844106e9a252433fc3a
hshristov18 <56866312+hshristov18@users.noreply.github.com>
2022-06-17 19:02:56 +0300

    deleting old project

--- src/utils/ApplicationProperties.java
+++
@@ -1,7 +0,0 @@
-package utils;
-
-public class ApplicationProperties {
-    public static final String JDBC_URL = "jdbc:sqlserver://DESKTOP-4DI9ATN;databaseName=SACP;encrypt=true;trustServerCertificate=true;integratedSecurity=true;";
-    public static final String USERNAME = "DESKTOP-4DI9ATN\\user";
-    public static final String PASSWORD = "";
-}
